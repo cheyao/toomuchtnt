477768df1dd09b34f8976a9e25843e200aaa52f7
cheyao <94928179+cheyao@users.noreply.github.com>
2024-10-26 16:28:37 +0200

    Init repo

---
+++ src/main/java/com/cyao/TooMuchTNTRevivedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cyao;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TooMuchTNTRevivedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
